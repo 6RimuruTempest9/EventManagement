1df84ad0d2fde2cedd96be36fbbb83757b1d1fc8
Vitali Talatai <Vitali_Talatai@epam.com>
2022-06-27 11:38:56 +0300

    VT_data-access-layer: added "EFDbContext" class; added "Microsoft.EntityFrameworkCore" library with dependencies from NuGet.

---
+++ src/EventManagement.DataAccess/Databases/EntityFrameworkCore/EFDbContext.cs
@@ -0,0 +1,35 @@
+﻿using EventManagement.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventManagement.DataAccess.Databases.EntityFrameworkCore
+{
+    internal class EFDbContext : DbContext
+    {
+        #region Constructors
+
+        public EFDbContext(DbContextOptions<EFDbContext> options)
+            : base(options)
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Event> Events { get; set; }
+
+        public DbSet<Organizer> Organizers { get; set; }
+
+        public DbSet<Speaker> Speakers { get; set; }
+
+        public DbSet<Street> Streets { get; set; }
+
+        public DbSet<Town> Towns { get; set; }
+
+        public DbSet<Venue> Venues { get; set; }
+
+        #endregion
+    }
+}
