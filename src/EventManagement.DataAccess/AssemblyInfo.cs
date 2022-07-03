b96a18279ac5b7b5efef148b67280280cea798d4
Vitali Talatai <Vitali_Talatai@epam.com>
2022-07-04 00:06:31 +0300

    VT_business-logic-layer: added assembly attribute between EventManagement.BusinessLogic and EventManagement.DataAccess projects.

---
+++ src/EventManagement.DataAccess/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b155e49a-4d15-4152-ac4d-ddfca40f23e9")]
+
+[assembly: InternalsVisibleTo("EventManagement.BusinessLogic")]
