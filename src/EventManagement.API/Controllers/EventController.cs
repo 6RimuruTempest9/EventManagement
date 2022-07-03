b5741824ddb1546f7692a69fa375a58cd8b7f738
Vitali Talatai <Vitali_Talatai@epam.com>
2022-07-04 01:52:26 +0300

    VT_api: removed unused classes; configured Program.cs and added EventController with work implementation.

---
+++ src/EventManagement.API/Controllers/EventController.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using EventManagement.API.Models;
+using EventManagement.BusinessLogic.DataTransferObjects;
+using EventManagement.BusinessLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventManagement.API.Controllers
+{
+    [ApiController]
+    [Route("events")]
+    public class EventController : Controller
+    {
+        #region Fields
+
+        private readonly IServiceAsync<EventDto> _eventService;
+        private readonly IMapper _mapper;
+
+        #endregion
+
+        #region Constructors
+
+        public EventController(
+            IServiceAsync<EventDto> eventService,
+            IMapper mapper)
+        {
+            _eventService = eventService;
+            _mapper = mapper;
+        }
+
+        #endregion
+
+        #region Methods
+
+        [HttpGet("getAll")]
+        public async Task<IEnumerable<Event>> GetAll()
+        {
+            return await _eventService
+                .GetAll()
+                .Select(eventDto => _mapper.Map<Event>(eventDto))
+                .ToListAsync();
+        }
+
+        [HttpGet("getById/{id}")]
+        public async Task<Event> GetById(int id)
+        {
+            var eventDto = await _eventService.GetByIdAsync(id);
+
+            return _mapper.Map<Event>(eventDto);
+        }
+
+        [HttpPost("delete/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _eventService.DeleteAsync(id);
+
+            return Ok();
+        }
+
+        [HttpPost("update")]
+        public async Task<IActionResult> Update([FromBody] Event @event)
+        {
+            var eventDto = _mapper.Map<EventDto>(@event);
+
+            await _eventService.UpdateAsync(eventDto);
+
+            return Ok();
+        }
+
+        [HttpPost("insert")]
+        public async Task<IActionResult> Insert([FromBody] Event @event)
+        {
+            var eventDto = _mapper.Map<EventDto>(@event);
+
+            await _eventService.InsertAsync(eventDto);
+
+            return Ok();
+        }
+
+        #endregion
+    }
+}
--- src/EventManagement.API/Program.cs
+++ src/EventManagement.API/Program.cs
@@ -1,15 +1,27 @@
+using EventManagement.BusinessLogic.Extensions;
+using EventManagement.DataAccess.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddEFDbContext(connectionString);
+
+builder.Services.AddEFRepositories();
+
+builder.Services.AddAutoMapper(typeof(EventManagement.BusinessLogic.Mappers.EventProfile));
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddServices();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
