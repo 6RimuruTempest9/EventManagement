ad04cdb6fd65552908bcbd09b82a0de3cbf3547a
Vitali Talatai <Vitali_Talatai@epam.com>
2022-07-04 00:05:51 +0300

    VT_business-logic-layer: added assembly attribute InternalsVisibleTo between EventManagement.DataAccess and EventManagement.BusinessLogic; added mapper for Event entity.

---
+++ src/EventManagement.BusinessLogic/Mappers/EventProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using EventManagement.BusinessLogic.DataTransferObjects;
+using EventManagement.DataAccess.Entities;
+
+namespace EventManagement.BusinessLogic.Mappers
+{
+    internal class EventProfile : Profile
+    {
+        #region Constructors
+
+        public EventProfile()
+        {
+            CreateMap<Event, EventDto>().ReverseMap();
+        }
+
+        #endregion
+    }
+}
