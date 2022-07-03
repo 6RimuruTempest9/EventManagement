5176a02bed4bf4f8f70fd1c9fac41cded07e328e
Vitali Talatai <Vitali_Talatai@epam.com>
2022-07-04 01:00:14 +0300

    VT_api: added extension for adding DbContext as DI component.

--- src/EventManagement.DataAccess/Extensions/IServiceCollectionExtensions.cs
+++ src/EventManagement.DataAccess/Extensions/IServiceCollectionExtensions.cs
@@ -1,12 +1,22 @@
-﻿using EventManagement.DataAccess.Entities;
+﻿using EventManagement.DataAccess.Databases.EntityFrameworkCore;
+using EventManagement.DataAccess.Entities;
 using EventManagement.DataAccess.Repositories;
 using EventManagement.DataAccess.Repositories.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EventManagement.DataAccess.Extensions
 {
     public static class IServiceCollectionExtensions
     {
+        public static void AddEFDbContext(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<EFDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+        }
+
         public static void AddEFRepositories(this IServiceCollection services)
         {
             services.AddScoped<IRepositoryAsync<Event>, EFEventRepository>();
