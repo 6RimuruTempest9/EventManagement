381d121e4add74a222d48b7b0fde9fae21de614a
Vitali Talatai <Vitali_Talatai@epam.com>
2022-07-04 00:33:02 +0300

    VT_business-logic-layer: added extensions for IServiceCollection interface that adding EventService as DI component.

---
+++ src/EventManagement.BusinessLogic/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using EventManagement.BusinessLogic.DataTransferObjects;
+using EventManagement.BusinessLogic.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventManagement.BusinessLogic.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IServiceAsync<EventDto>, EventService>();
+        }
+    }
+}
