cf194388bcf8fac9175e6a4f8ea98a7cc5932e2b
Vitali Talatai <Vitali_Talatai@epam.com>
2022-07-04 01:03:25 +0300

    VT_api: added Event mapper profile.

---
+++ src/EventManagement.API/Mappers/EventProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using EventManagement.API.Models;
+using EventManagement.BusinessLogic.DataTransferObjects;
+
+namespace EventManagement.API.Mappers
+{
+    internal class EventProfile : Profile
+    {
+        #region Constructors
+
+        public EventProfile()
+        {
+            CreateMap<EventDto, Event>().ReverseMap();
+        }
+
+        #endregion
+    }
+}
