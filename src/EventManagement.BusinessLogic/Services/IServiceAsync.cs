bfac883c9d71089c2b92dd03fc5acce0576e8c6f
Vitali Talatai <Vitali_Talatai@epam.com>
2022-07-04 00:13:15 +0300

    VT_business-logic-layer: added asynchronious service interface.

---
+++ src/EventManagement.BusinessLogic/Services/IServiceAsync.cs
@@ -0,0 +1,18 @@
+﻿using EventManagement.BusinessLogic.DataTransferObjects;
+
+namespace EventManagement.BusinessLogic.Services
+{
+    public interface IServiceAsync<TDto>
+        where TDto : IDto
+    {
+        public IQueryable<TDto> GetAll();
+
+        public Task<TDto> GetByIdAsync(int id);
+
+        public Task InsertAsync(TDto dto);
+
+        public Task UpdateAsync(TDto dto);
+
+        public Task DeleteAsync(int id);
+    }
+}
