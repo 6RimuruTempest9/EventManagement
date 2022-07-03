b63109624f8e5904785ef0301f6cdcd65ef13a51
Vitali Talatai <Vitali_Talatai@epam.com>
2022-07-04 01:01:03 +0300

    VT_api: added Event model.

---
+++ src/EventManagement.API/Models/Event.cs
@@ -0,0 +1,25 @@
+﻿namespace EventManagement.API.Models
+{
+    internal class Event
+    {
+        #region Properties
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Venue { get; set; }
+
+        public string Organizer { get; set; }
+
+        public string Speaker { get; set; }
+
+        #endregion
+    }
+}
